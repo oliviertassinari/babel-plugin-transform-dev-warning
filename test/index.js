1d3860a2c7e1bd6ea1b57704cadba9c24565dd0d
Olivier Tassinari <olivier.tassinari@gmail.com>
2015-12-24 19:09:49 +0100

    [test] Use the right name

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ function trim(str) {
   return str.replace(/^\s+|\s+$/, '');
 }
 
-describe('remove react propTypes', () => {
+describe('transform warning call', () => {
   const fixturesDir = path.join(__dirname, 'fixtures');
   fs.readdirSync(fixturesDir).map((caseName) => {
     it(`should ${caseName.split('-').join(' ')}`, () => {
